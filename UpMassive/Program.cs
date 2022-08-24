3134252986b42ba9cf41f1984fcdbe1ed0110d42
Liudmila <vocrug-solnza@mail.ru>
2022-08-24 18:07:48 +0300

    Цикл

---
+++ UpMassive/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] array = { 1, 2, 4, 4, 5, 6, 7, 8};
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+if ( array[index] == find)
+
+{
+    Console.WriteLine(index);
+    break;
+    }
+   
+    index++;
+
+}
